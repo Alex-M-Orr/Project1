c3f53e797033c3a524ba9da6a782068e20b2e1b9
Alex Orr <orr.m.alexander@gmail.com>
2020-10-28 21:08:19 -0500

    adding data routes

--- src/main/java/com/projectone/servlet/RequestForwarder.java
+++ src/main/java/com/projectone/servlet/RequestForwarder.java
@@ -56,6 +56,14 @@ public class RequestForwarder {
 			break;
 		case "/Project1/userinfo.json":
 			new UsersDataController().getSessionData(res, req);
+		case "/Project1-alpha/userslist.json":
+			new UsersDataController().sendAllData(res);
+			break;
+		case "/Project1-alpha/reimbursementlist.json":
+			new ReimbursementDataController().sendAllData(res);
+			break;
+		case "/Project1-alpha/userinfo.json":
+			new UsersDataController().getSessionData(res, req);
 		}
 	}
 }
